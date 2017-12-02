deb4db59d0fc9fbcb8b20d7a739ca06cb28b20b9
Sandeep <sandeep_gandham@tecnics.com>
2017-12-02 16:52:56 +0530

    Access token done

--- src/main/java/com/example/FileSystemAdapter/Controller/Login.java
+++ src/main/java/com/example/FileSystemAdapter/Controller/Login.java
@@ -1,5 +1,6 @@
 package com.example.FileSystemAdapter.Controller;
 
+import com.example.FileSystemAdapter.Service.EsalidaOauthService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Login {
 
+    private EsalidaOauthService esalidaOauthService;
+
+    public Login(EsalidaOauthService esalidaOauthService){
+        this.esalidaOauthService=esalidaOauthService;
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@RequestParam ("username") String userId, @RequestParam("password") String password){
-        return null;
+    public String login(@RequestParam ("username") String username, @RequestParam("password") String password){
+
+        String accessToken = esalidaOauthService.login(username, password);
+
+        return accessToken;
     }
 
 }
--- src/main/java/com/example/FileSystemAdapter/Service/EsalidaOauthServiceImpl.java
+++ src/main/java/com/example/FileSystemAdapter/Service/EsalidaOauthServiceImpl.java
@@ -1,19 +1,50 @@
 package com.example.FileSystemAdapter.Service;
 
+import com.example.FileSystemAdapter.Models.AccessToken;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EsalidaOauthServiceImpl implements EsalidaOauthService {
 
+    private RestTemplate restTemplate;
+
     @Value("${esalida-oauth-base-url}")
     String esalidaOauthBaseUrl;
+
+    private  String tokenEndpoint= "/oauth/token";
+
+    private  String grantType="password";
+
+    public EsalidaOauthServiceImpl(){
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setBufferRequestBody(false);
+        this.restTemplate = new RestTemplate(requestFactory);
+
+    }
+
 
     @Override
     public String login(String username, String password) {
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic b2F1dGgtY2xpZW50OnNlY3JldA==");
+        HttpEntity requestEntity = new HttpEntity(headers);
+        String loginUrl = esalidaOauthBaseUrl+tokenEndpoint+"?username="+username+"&password="+password+"&grant_type="+grantType;
 
+       ResponseEntity<AccessToken> result = restTemplate.exchange(loginUrl, HttpMethod.POST, requestEntity, AccessToken.class);
 
-        return null;
+        //ResponseEntity<AccessToken> result = restTemplate.exchange("localhost:4001/esalida/oauth/oauth/token?username=santhosh&password=password&grant_type=password", HttpMethod.POST, requestEntity, AccessToken.class);
+
+        //ResponseEntity<AccessToken> result =restTemplate.postForEntity("http://localhost:4001/esalida/oauth/oauth/token?username=santhosh&password=password&grant_type=password", requestEntity, AccessToken.class);
+
+        return result.getBody().getAccess_token();
     }
 }
